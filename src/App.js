6bd2564014c23220d07a455335e72694b372bd92
anand <anandvipul01@gmail.com>
2023-07-14 16:53:35 +0530

    project-setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from '@mui/material';
 function App() {
   return (
     <div className="App">
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button variant="contained">Click Me!</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
