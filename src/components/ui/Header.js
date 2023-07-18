9425d95ba095e2b5daf4300b9a99836b37bdd0f9
anand <anandvipul01@gmail.com>
2023-07-19 00:28:30 +0530

    tab refresh fix

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import { useScrollTrigger } from '@mui/material';
@@ -28,7 +28,10 @@ const BottomSpacer = styled('div')(({ theme }) => ({
 }));
 
 const Logo = styled('img')(({ theme }) => ({
-  height: '7em',
+  height: '8em',
+  '&.hover': {
+    backgroundColor: 'transparent',
+  },
 }));
 
 const TabContainer = styled(Tabs)(({ theme }) => ({
@@ -58,12 +61,36 @@ export default function Header(props) {
     setValue(value);
   };
 
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4);
+    }
+  }, [value]);
+
   return (
     <>
       <ElevationScroll>
         <AppBar position="fixed" color="primary">
           <Toolbar disableGutters={true}>
-            <Logo alt="company logo" src={logo} />
+            <Button
+              disableRipple
+              component={Link}
+              to="/"
+              sx={{ padding: 0 }}
+              onClick={() => {
+                setValue(0);
+              }}
+            >
+              <Logo alt="company logo" src={logo} />
+            </Button>
 
             <TabContainer
               value={value}
